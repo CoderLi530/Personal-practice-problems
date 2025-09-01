b79dc7ca9ea7b821d0417d696243ee9a76dda344
CoderLi530 <Haopeoplezz@outlook.com>
2025-09-01 18:10:32 +0800

    二叉树的最近公共祖先

--- 二叉树/二叉树的最近公共祖先/代码.cpp
+++ 二叉树/二叉树的最近公共祖先/代码.cpp
@@ -10,36 +10,62 @@
 class Solution
 {
 public:
+
+    //将节点的父节点存储在哈希表中
+    void dfs(TreeNode* root, unordered_map<int, TreeNode*>& hash)
+    {
+        if (root == nullptr) return ; 
+        if (root->left)
+        {
+            hash[root->left->val] = root;
+            dfs(root->left, hash);
+        }
+        if (root->right)
+        {
+            hash[root->right->val] = root;
+            dfs(root->right, hash);
+        }
+    }
+
+    //递归
+    TreeNode* _lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (root == nullptr) return  nullptr;
+        if (root == p || root == q) return root;
+        TreeNode* node1 = _lowestCommonAncestor(root->left, p, q);
+        TreeNode* node2 = _lowestCommonAncestor(root->right, p, q);  
+        //p和q找到了，当前root就是最近公共祖先
+        if (node1 && node2) return root;
+        //p，q没找到，返回另一个
+        return node1 ? node1 : node2;
+    }
+
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        // //使用栈存储节点，然后遍历栈，找到最近公共祖先
-        // //如果两个目标节点同时进栈，说明公共祖先不是目标节点，否则先进栈的目标节点就是最近公共祖先
-        // queue<TreeNode*> que;
-        // que.push(root);
-        // while (!que.empty())
+        // //存储父节点
+        // //时间复杂度O(N) 空间复杂度O(N)
+        // unordered_map<int, TreeNode*> hash; //存储每个节点的父节点
+        // unordered_map<int, bool> vis; //记录已访问过的父节点
+        // //1. 使用哈希表存储每个节点的父节点
+        // hash[root->val] = nullptr; //根节点的父节点为空
+        // dfs(root, hash);
+        // //2. 从p节点开始访问其父节点，使用哈希表存储已经访问过的父节点
+        // while (p)
         // {
-        //     int n = que.size();
-        //     while (n--)
-        //     {
-        //         TreeNode* node = que.front();
-        //         if (node == p || node == q) return node;
-        //         if (node->left) que.push(node->left);
-        //         if (node->right) que.push(node->right);
-        //         if (node->left || node->right)
-        //         {
-        //             if (node->left && (node->left == p || node->left == q))
-        //             {
-        //                 if (node->right && (node->right == p || node->right == q))
-        //                 {
-        //                    return node; 
-        //                 }
-        //                 else return node->left;
-        //             }
-        //             if (node->right == p || node->right == q) return node->right;
-        //         }
-        //         que.pop();
-        //     }
+        //     vis[p->val] = true;
+        //     p = hash[p->val];
+        // }
+        // //3. 从q节点开始访问其父节点，在哈希表中查找，如果存在，说明该节点就是最近公共祖先
+        // while (q)
+        // {
+        //     if (vis[q->val]) return q;
+        //     q = hash[q->val];
         // }
         // return nullptr;
+
+
+        //dfs
+        //时间复杂度O(N) 空间复杂度O(N)
+        return _lowestCommonAncestor(root, p, q);
     }
 };
