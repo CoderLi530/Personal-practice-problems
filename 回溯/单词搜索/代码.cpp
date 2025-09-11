bd0e00badc7e1c501fc4472c1634f5580a1ba4a6
CoderLi530 <Haopeoplezz@outlook.com>
2025-09-11 21:27:40 +0800

    单词搜索

--- 回溯/单词搜索/代码.cpp
+++ 回溯/单词搜索/代码.cpp
@@ -16,7 +16,6 @@ public:
                 if (count == strlen(str))
                 {
                     flag = true;
-                    cout << "4" << flag << endl;
                     return ;
                 }
                 count--;
@@ -34,7 +33,6 @@ public:
                 if (count == strlen(str))
                 {
                     flag = true;
-                    cout << "4" << flag << endl;
                     return ;
                 }
                 count--;
@@ -52,7 +50,6 @@ public:
                 if (count == strlen(str))
                 {
                     flag = true;
-                    cout << "4" << flag << endl;
                     return ;
                 }
                 count--;
@@ -70,7 +67,6 @@ public:
                 if (count == strlen(str))
                 {
                     flag = true;
-                    cout << "4" << flag << endl;
                     return ;
                 }
                 count--;
@@ -81,18 +77,78 @@ public:
 
     bool exist(vector<vector<char>> &board, string word)
     {
-        int m = board.size(), n = board[0].size();
-        const char* str = word.c_str();
-        vector<vector<bool>> vis(m, vector(n, false));
-        vector<pair<int, int>> search;
+        //回溯
+
+        int m = board.size(), n = board[0].size(); //行列
+        const char* str = word.c_str(); //字符串
+        vector<vector<bool>> vis(m, vector(n, false)); //标记数组
+        vector<pair<int, int>> search; //记录word中第一个字母在board出现位置的下标
         for (int i = 0; i < m; ++i)
         {
             for (int j = 0; j < n; ++j) if (board[i][j] == str[0]) search.push_back({i, j});
         }      
-        bool flag = false;
+        bool flag = false; //标记位
         for (auto& [x, y] : search)
         {
             backtrack(board, str, vis, flag, x, y, 0, 0);
+        }
+        return flag;
+    }
+};
+
+
+//优化后
+class Solution 
+{
+public:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    bool flag = false;
+
+    void backtrack(vector<vector<char>>& board, const char* str, vector<vector<bool>>& vis, int x, int y, int len, int start)
+    {
+        if (flag) return ;
+        //匹配完成，设置flag并进行返回
+        if (start == len) 
+        {
+            flag = true;
+            return ;
+        }
+        //走不同方向
+        for (int i = 0; i < 4; ++i)
+        {
+            int a = x + dx[i];
+            int b = y + dy[i];
+            if (a >= 0 && a < board.size() && b >= 0 && b < board[0].size() && !flag && !vis[a][b] && board[a][b] == str[start])
+            {
+                //标记该位置已访问，不走重复的分支
+                vis[a][b] = true;
+                backtrack(board, str, vis, a, b, len, start + 1);
+                vis[a][b] = false;
+            }
+        }
+    }
+
+    bool exist(vector<vector<char>>& board, string word) 
+    {
+        //回溯
+        //时间复杂度O(M*N*3^L) 空间复杂度O(MN)
+        int m = board.size(), n = board[0].size(); //
+        const char* str = word.c_str();
+        vector<vector<bool>> vis(m, vector(n, false));
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (board[i][j] == str[0])
+                {
+                    vis[i][j] = true;
+                    backtrack(board, str, vis, i, j, strlen(str), 1);
+                    vis[i][j] = false;
+                    //进行剪枝
+                    if (flag) return flag;
+                } 
+            } 
         }
         return flag;
     }
